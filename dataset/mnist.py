9d4f9cb88137fee202586c2535213e72374a47a2
jjarajjara <jjarajjara1903@naver.com>
2024-02-20 17:32:36 +0900

    커밋

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 try:
     import urllib.request
 except ImportError:
@@ -29,46 +28,50 @@ img_size = 784
 
 def _download(file_name):
     file_path = dataset_dir + "/" + file_name
-    
+
     if os.path.exists(file_path):
         return
 
     print("Downloading " + file_name + " ... ")
-    urllib.request.urlretrieve(url_base + file_name, file_path)
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+    request = urllib.request.Request(url_base+file_name, headers=headers)
+    response = urllib.request.urlopen(request).read()
+    with open(file_path, mode='wb') as f:
+        f.write(response)
     print("Done")
-    
+
 def download_mnist():
     for v in key_file.values():
        _download(v)
-        
+
 def _load_label(file_name):
     file_path = dataset_dir + "/" + file_name
-    
+
     print("Converting " + file_name + " to NumPy Array ...")
     with gzip.open(file_path, 'rb') as f:
             labels = np.frombuffer(f.read(), np.uint8, offset=8)
     print("Done")
-    
+
     return labels
 
 def _load_img(file_name):
     file_path = dataset_dir + "/" + file_name
-    
-    print("Converting " + file_name + " to NumPy Array ...")    
+
+    print("Converting " + file_name + " to NumPy Array ...")
     with gzip.open(file_path, 'rb') as f:
             data = np.frombuffer(f.read(), np.uint8, offset=16)
     data = data.reshape(-1, img_size)
     print("Done")
-    
+
     return data
-    
+
 def _convert_numpy():
     dataset = {}
     dataset['train_img'] =  _load_img(key_file['train_img'])
-    dataset['train_label'] = _load_label(key_file['train_label'])    
+    dataset['train_label'] = _load_label(key_file['train_label'])
     dataset['test_img'] = _load_img(key_file['test_img'])
     dataset['test_label'] = _load_label(key_file['test_label'])
-    
+
     return dataset
 
 def init_mnist():
@@ -83,45 +86,45 @@ def _change_one_hot_label(X):
     T = np.zeros((X.size, 10))
     for idx, row in enumerate(T):
         row[X[idx]] = 1
-        
+
     return T
-    
+
 
 def load_mnist(normalize=True, flatten=True, one_hot_label=False):
-    """MNIST 데이터셋 읽기
-    
+    """MNISTデータセットの読み込み
+
     Parameters
     ----------
-    normalize : 이미지의 픽셀 값을 0.0~1.0 사이의 값으로 정규화할지 정한다.
-    one_hot_label : 
-        one_hot_label이 True면、레이블을 원-핫(one-hot) 배열로 돌려준다.
-        one-hot 배열은 예를 들어 [0,0,1,0,0,0,0,0,0,0]처럼 한 원소만 1인 배열이다.
-    flatten : 입력 이미지를 1차원 배열로 만들지를 정한다. 
-    
+    normalize : 画像のピクセル値を0.0~1.0に正規化する
+    one_hot_label :
+        one_hot_labelがTrueの場合、ラベルはone-hot配列として返す
+        one-hot配列とは、たとえば[0,0,1,0,0,0,0,0,0,0]のような配列
+    flatten : 画像を一次元配列に平にするかどうか
+
     Returns
     -------
-    (훈련 이미지, 훈련 레이블), (시험 이미지, 시험 레이블)
+    (訓練画像, 訓練ラベル), (テスト画像, テストラベル)
     """
     if not os.path.exists(save_file):
         init_mnist()
-        
+
     with open(save_file, 'rb') as f:
         dataset = pickle.load(f)
-    
+
     if normalize:
         for key in ('train_img', 'test_img'):
             dataset[key] = dataset[key].astype(np.float32)
             dataset[key] /= 255.0
-            
+
     if one_hot_label:
         dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
-        dataset['test_label'] = _change_one_hot_label(dataset['test_label'])    
-    
+        dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
+
     if not flatten:
          for key in ('train_img', 'test_img'):
             dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
 
-    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label']) 
+    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
 
 
 if __name__ == '__main__':
