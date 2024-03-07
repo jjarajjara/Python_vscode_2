534b426f451bd6d071740705db11dea356861164
jjarajjara <jjarajjara1903@naver.com>
2024-03-07 14:23:19 +0900

    commit

--- Python_Test1.py
+++ Python_Test1.py
@@ -159,6 +159,7 @@
 ### 다차원 배열인 경우 ) 배열의 갯수, 배열 안의 원소 갯수 
 ### 1차원 배열인 경우 ) 배열 안의 원소 갯수, (공백)
 
+# import numpy as np
 
 # B = np.array([[1,2], [3,4], [5,6]])
 # print(B)
@@ -1149,25 +1150,34 @@
     
 ## gradient check ( 기울기 확인 )
     
-import sys, os
-sys.path.append(os.pardir)
-import numpy as np
-from dataset.mnist import load_mnist
-from two_layer_net import TwoLayerNet
+# import sys, os
+# sys.path.append(os.pardir)
+# import numpy as np
+# from dataset.mnist import load_mnist
+# from two_layer_net import TwoLayerNet
 
 
-## 데이터 읽기
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+# ## 데이터 읽기
+# (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+# network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-x_batch = x_train[:3]
-t_batch = t_train[:3]
+# x_batch = x_train[:3]
+# t_batch = t_train[:3]
 
-grad_numerical = network.numerical_gradient(x_batch, t_batch)
-grad_backprop = network.gradient(x_batch, t_batch)
+# grad_numerical = network.numerical_gradient(x_batch, t_batch)
+# grad_backprop = network.gradient(x_batch, t_batch)
 
-## 각 가중치의 차이의 절댓값을 구한 후, 그 절댓값들의 평균을 낸다.
-for key in grad_numerical.keys():
-    diff = np.average(np.abs(grad_backprop[key] - grad_numerical[key]))
-    print(key + ":" + str(diff))
+# ## 각 가중치의 차이의 절댓값을 구한 후, 그 절댓값들의 평균을 낸다.
+# for key in grad_numerical.keys():
+#     diff = np.average(np.abs(grad_backprop[key] - grad_numerical[key]))
+#     print(key + ":" + str(diff))
+
+
+## CH07
+## 합성곱 신경망 (Convolutional Neural Network, CNN)
+
+## CNN의 구성 요소
+## 합성곱 계층 (convolutional layer)
+## 풀링 계층 (pooling layer)
+
